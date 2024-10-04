fb55d6f9837be48b4d8978107cba78d15b83216e
Sh-karan27 <shuklakaran107@gmail.com>
2024-10-04 19:27:23 +0530

    tailind config chnages

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    background: {
+      primary: '#151517',
+      secondary: '#202120',
+    },
+    colors: {
+      main: '#63CC8E',
+    },
   },
   plugins: [],
 };
