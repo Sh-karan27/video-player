54a5cacbf5a4154f1e762924e443fc9149589a36
Sh-karan27 <shuklakaran107@gmail.com>
2024-10-15 01:17:45 +0530

    cc

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,23 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './store/index.js';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-      <App />
+    <App />
+    <ToastContainer
+      position='bottom-left'
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme='dark'
+      transition:Bounce
+    />
   </Provider>
 );
